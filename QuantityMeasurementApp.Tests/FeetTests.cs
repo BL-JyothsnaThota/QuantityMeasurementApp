feef0d4d1995d4a84269ea016822e67e602cc400
BL-JyothsnaThota <jyothsna.thota@bridgelabz.com>
2026-02-06 14:55:35 +0530

    [Jyothsna] UC1: Feet measurement equality

---
+++ QuantityMeasurementApp.Tests/FeetTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantityMeasurementApp.Tests
+{
+    public class FeetTests
+    {
+        [Fact]
+        public void GivenSameFeetValues_WhenCompared_ShouldReturnTrue()
+        {
+            var feet1 = new Feet(1.0);
+            var feet2 = new Feet(1.0);
+
+            Assert.True(feet1.Equals(feet2));
+        }
+
+        [Fact]
+        public void GivenDifferentFeetValues_WhenCompared_ShouldReturnFalse()
+        {
+            var feet1 = new Feet(1.0);
+            var feet2 = new Feet(2.0);
+
+            Assert.False(feet1.Equals(feet2));
+        }
+
+        [Fact]
+        public void GivenFeet_WhenComparedWithNull_ShouldReturnFalse()
+        {
+            var feet = new Feet(1.0);
+
+            Assert.False(feet.Equals(null));
+        }
+
+        [Fact]
+        public void GivenSameFeetReference_WhenCompared_ShouldReturnTrue()
+        {
+            var feet = new Feet(1.0);
+
+            Assert.True(feet.Equals(feet));
+        }
+
+        [Fact]
+        public void GivenFeet_WhenComparedWithDifferentType_ShouldReturnFalse()
+        {
+            var feet = new Feet(1.0);
+            var notFeet = 1.0;
+
+            Assert.False(feet.Equals(notFeet));
+        }
+    }
+}
---
+++ QuantityMeasurementApp/Feet.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantityMeasurementApp
+{
+    public class Feet
+    {
+        private readonly double _value;
+
+        public Feet(double value)
+        {
+            _value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (obj is null)
+                return false;
+
+            if (obj.GetType() != typeof(Feet))
+                return false;
+
+            var other = (Feet)obj;
+            return _value.Equals(other._value);
+        }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+    }
+}
