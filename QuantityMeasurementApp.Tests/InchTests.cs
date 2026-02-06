c7fc0ef4187fbe06fe8c54fdfe29e67a2aec969d
BL-JyothsnaThota <jyothsna.thota@bridgelabz.com>
2026-02-06 15:17:55 +0530

    [Jyothsna] UC2: Feet and Inches measurement equality

---
+++ QuantityMeasurementApp.Tests/InchTests.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantityMeasurementApp.Tests
+{
+    public class InchTests
+    {
+        [Fact]
+        public void GivenSameInchValues_WhenCompared_ShouldReturnTrue()
+        {
+            var inch1 = new Inch(1.0);
+            var inch2 = new Inch(1.0);
+
+            Assert.True(inch1.Equals(inch2));
+        }
+
+        [Fact]
+        public void GivenDifferentInchValues_WhenCompared_ShouldReturnFalse()
+        {
+            var inch1 = new Inch(1.0);
+            var inch2 = new Inch(2.0);
+
+            Assert.False(inch1.Equals(inch2));
+        }
+
+        [Fact]
+        public void GivenInch_WhenComparedWithNull_ShouldReturnFalse()
+        {
+            var inch = new Inch(1.0);
+
+            Assert.False(inch.Equals(null));
+        }
+
+        [Fact]
+        public void GivenSameInchReference_WhenCompared_ShouldReturnTrue()
+        {
+            var inch = new Inch(1.0);
+
+            Assert.True(inch.Equals(inch));
+        }
+
+        [Fact]
+        public void GivenInch_WhenComparedWithDifferentType_ShouldReturnFalse()
+        {
+            var inch = new Inch(1.0);
+
+            Assert.False(inch.Equals(new Feet(1.0)));
+        }
+    }
+}
---
+++ QuantityMeasurementApp/Inch.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace QuantityMeasurementApp
+{
+    public class Inch
+    {
+        private readonly double _value;
+
+        public Inch(double value)
+        {
+            _value = value;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(this, obj))
+                return true;
+
+            if (obj is null)
+                return false;
+
+            if (obj.GetType() != typeof(Inch))
+                return false;
+
+            var other = (Inch)obj;
+            return _value.Equals(other._value);
+        }
+
+        public override int GetHashCode()
+        {
+            return _value.GetHashCode();
+        }
+    }
+}
